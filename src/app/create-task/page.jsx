254022f1704a67c8f37124c619cd274c68c9a690
amiribnjaman <amiribnjaman@gmail.com>
2023-11-07 16:00:38 +0600

    Create task ux simply upgrade

--- src/app/create-task/page.jsx
+++ src/app/create-task/page.jsx
@@ -3,10 +3,13 @@
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { SearchContext } from "../context/SearchContext";
 
 export default function page() {
   const navigate = useRouter();
+  // SERACH CONTEXT VALUE
+  const { setReload, reload } = useContext(SearchContext);
 
   // Check token and if haven't the token then push to login page
   let token;
@@ -44,6 +47,7 @@ export default function page() {
         .then((data) => {
           if (data.status == "201") {
             toast.success("A Task created succefully!");
+            setReload(!reload)
           } else {
             toast.error(data.msg);
           }
